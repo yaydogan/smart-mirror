4e4aa91f6cda34d8b4f12bf0133a054448fcb390
Evan Cohen <evanbtcohen@gmail.com>
2016-02-09 01:36:28 -0800

    functioning mirror on android via cordova. lots of bugs still

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,6 +1,58 @@
 (function(angular) {
     'use strict';
-
+    
+    //Add Smart Mirror Module
     angular.module('SmartMirror', ['ngAnimate']);
+    
+    var MirrorInit = function() {
+
+        var onDeviceReady = function() {
+            //Check for media permissions
+            var src = "myrecording.mp3";
+            var media = new Media(src,
+                // We have sufficient permissions
+                function() {
+                    console.log("recordAudio():Audio Success");
+                    media.stopRecord();
+                    media.release();
+                },
+                // We don't have the correct permissions
+                function(err) {
+                    console.log("recordAudio():Audio Error: "+ err.code);
+                    alert("The smart mirror needs Audio and File permissions to run. Grant these permissions and restart the app.");
+                });
+
+            media.setVolume(0);
+            media.startRecord();
+            media.stopRecord();
+            media.release();
+            
+            receivedEvent('deviceready');
+        };
+
+        var receivedEvent = function(event) {
+            console.log('Start event received, bootstrapping application setup.');
+            angular.bootstrap(document, ['SmartMirror']);
+        };
+
+        this.bindEvents = function() {
+            document.addEventListener('deviceready', onDeviceReady, false);
+        };
+
+        //If cordova is present, wait for it to initialize, otherwise just try to
+        //bootstrap the application.
+        if (window.cordova !== undefined) {
+            console.log('Cordova found, wating for device.');
+            this.bindEvents();
+        } else {
+            angular.element(document).ready(function() {
+                console.log('Cordova not found, booting application');
+                receivedEvent('manual');
+            });
+        }
+    };
+    
+    console.log('Bootstrapping!');
+	new MirrorInit();
 
 }(window.angular));
--- platforms/android/assets/www/js/config.js
+++ platforms/android/assets/www/js/config.js
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream:platforms/android/assets/www/js/config.js
-var FORCAST_API_KEY = "{YOUR_KEY_HERE}";
-var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
-=======
 var FORCAST_API_KEY = "df68c3a0fd4b1a73d95b4ee10ff068bb";
 var HUE_BASE = "http://192.168.1.11/api/195a3167224715df20354b576beb1cf/";
-var PERSONAL_CALENDAR = ["https://calendar.google.com/calendar/ical/evanbtcohen%40gmail.com/private-e6a6e094dd1267b108cc04686f3f2450/basic.ics", "https://outlook.office365.com/owa/calendar/99a95276e9774fbca1225463cbf8ccd1@microsoft.com/de6ea77615bc47d09f1b9363fb2676541083067335678426766/S-1-8-3811523688-4180214211-873983030-2881102700/reachcalendar.ics"];
->>>>>>> Stashed changes:js/config.js
+var PERSONAL_CALENDAR = ["https://calendar.google.com/calendar/ical/evanbtcohen%40gmail.com/private-e6a6e094dd1267b108cc04686f3f2450/basic.ics"];
