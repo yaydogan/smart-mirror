e7bc883ac4e92cdc100f7cf4df0302d8b5b05dc5
Evan Cohen <evanbtcohen@gmail.com>
2016-02-09 01:52:04 -0800

    adding calendar to config

--- platforms/android/assets/www/js/config.js
+++
@@ -1,3 +0,0 @@
-var FORCAST_API_KEY = "df68c3a0fd4b1a73d95b4ee10ff068bb";
-var HUE_BASE = "http://192.168.1.11/api/195a3167224715df20354b576beb1cf/";
-var PERSONAL_CALENDAR = ["https://calendar.google.com/calendar/ical/evanbtcohen%40gmail.com/private-e6a6e094dd1267b108cc04686f3f2450/basic.ics"];
